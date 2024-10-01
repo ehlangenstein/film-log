ce2bb6e4537d53fb279270c41fe58079b127bba9
Elizabeth Langenstein <elizabeth.langenstein@kellogg.northwestern.edu>
2024-10-01 20:54:53 +0000

    fixed genre sync

--- app/services/tmdb_genre_service.rb
+++ app/services/tmdb_genre_service.rb
@@ -1,14 +1,30 @@
 require 'uri'
 require 'net/http'
+require 'json'
 
-url = URI("https://api.themoviedb.org/3/genre/movie/list?language=en")
+class TmdbGenreService
+  TMDB_API_URL = "https://api.themoviedb.org/3/genre/movie/list?language=en"
+  TMDB_API_KEY = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMzRlOGFkYWU5ZWM1MzZkYzUzYzNjMzI1ZTc4MjgwMSIsIm5iZiI6MTcyNzgwNzgyMS41OTMzNjEsInN1YiI6IjY2ZjQ3Y2ExZjViNDk3ODY0MzIzMjUwMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IAJLJU5n1UaYm1ZKhwQRhoOvcm4KNR4z7HD3hZfZzuE"
 
-http = Net::HTTP.new(url.host, url.port)
-http.use_ssl = true
+  def self.fetch_genres
+    url = URI(TMDB_API_URL)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
 
-request = Net::HTTP::Get.new(url)
-request["accept"] = 'application/json'
-request["Authorization"] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMzRlOGFkYWU5ZWM1MzZkYzUzYzNjMzI1ZTc4MjgwMSIsIm5iZiI6MTcyNzgwNzgyMS41OTMzNjEsInN1YiI6IjY2ZjQ3Y2ExZjViNDk3ODY0MzIzMjUwMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IAJLJU5n1UaYm1ZKhwQRhoOvcm4KNR4z7HD3hZfZzuE'
+    request = Net::HTTP::Get.new(url)
+    request["accept"] = 'application/json'
+    request["Authorization"] = TMDB_API_KEY
 
-response = http.request(request)
-puts response.read_body
+    response = http.request(request)
+    genres_data = JSON.parse(response.read_body)
+
+    if genres_data["genres"]
+      genres_data["genres"].each do |genre|
+        Genre.find_or_create_by(genre_id: genre["id"], name: genre["name"])
+      end
+    else
+      puts "Error fetching genres"
+    end
+  end
+end
+
--- db/migrate/20241001203939_create_genres.rb
+++ db/migrate/20241001203939_create_genres.rb
@@ -1,8 +1,9 @@
 class CreateGenres < ActiveRecord::Migration[7.1]
   def change
     create_table :genres do |t|
-      t.integer id 
-      t.string name
+      t.integer :genre_id
+      t.string :name
+
       t.timestamps
     end
   end
